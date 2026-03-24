dab6d18d95cfabdcedf5585c2b46455dc07448bd
Hiruy Legesse <hiruyadane@gmail.com>
2026-03-24 11:09:05 +0300

    Fix firebase admin initialization env vars

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -4,13 +4,17 @@ import { getFirestore } from "firebase-admin/firestore";
 
 const app = getApps();
 
-const intiFirebaseAdmin = () => {
+const initFirebaseAdmin = () => {
   if (!app.length) {
+    if (!process.env.FIREBASE_PROJECT_ID) throw new Error('FIREBASE_PROJECT_ID is not defined');
+    if (!process.env.FIREBASE_CLIENT_EMAIL) throw new Error('FIREBASE_CLIENT_EMAIL is not defined');
+    if (!process.env.FIREBASE_PRIVATE_KEY) throw new Error('FIREBASE_PRIVATE_KEY is not defined');
+
     initializeApp({
       credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
       }),
     });
   } else {
@@ -18,7 +22,7 @@ const intiFirebaseAdmin = () => {
   }
 };
 
-intiFirebaseAdmin();
+initFirebaseAdmin();
 
 export const auth = getAuth(getApp());
 export const db = getFirestore(getApp());
