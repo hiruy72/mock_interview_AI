f88958d95d1ddd67991c7658bd55be7ed4ff72f0
Hiruy Legesse <hiruyadane@gmail.com>
2026-03-24 11:14:41 +0300

    Revert to camelCase props for firebase admin cert

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -12,9 +12,9 @@ const initFirebaseAdmin = () => {
 
     initializeApp({
       credential: cert({
-        project_id: process.env.FIREBASE_PROJECT_ID,
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
-        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
       }),
     });
   } else {
